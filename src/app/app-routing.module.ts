b55ef5d90962371c5df8fe51c1a0ef610fbab4b9
Thong Bui <buiquangthong@gmail.com>
2019-12-28 21:29:58 +0700

    Add sample pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+
+import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { 
+    path: '', 
+    component: FullLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
+import { TableComponent } from './components/table/table.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    FullLayoutComponent,
+    TableComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
